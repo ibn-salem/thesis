b28b05a41ace658d2389d466e4b767c19e12429a
ibn-salem <jonas.ibn-salem@gmx.de>
2018-01-30 23:00:50 +0100

    add travis settings

--- _deploy.sh
+++ _deploy.sh
@@ -5,12 +5,12 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
+git config --global user.email "j.ibn-salem@uni-mainz.de"
+git config --global user.name "Jonas Ibn-Salem (via travis)"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
-cp -r ../_book/* ./
+cp -r ../doc/* ./
 git add --all *
 git commit -m"Update the book" || true
 git push -q origin gh-pages
