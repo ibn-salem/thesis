ea0098229defbb8667f7085bdadacced6d991a03
ibn-salem <jonas.ibn-salem@gmx.de>
2018-01-31 16:16:46 +0100

    no cleaning between build of different formats

--- _build.sh
+++ _build.sh
@@ -4,7 +4,7 @@
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 
 # build pdf version
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book', clean = FALSE)"
 
 # build epub version
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book', clean = FALSE)"
