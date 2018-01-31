47dcdf7b94bbdde98b9edc7af14c72f37e5c4ae4
ibn-salem <jonas.ibn-salem@gmx.de>
2018-01-31 19:26:22 +0100

    trigger build

--- _build.sh
+++ _build.sh
@@ -2,5 +2,7 @@
 
 # build book in seprate R sessions for all output formats
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
