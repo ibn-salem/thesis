8cbc97f0a5f73c1580090f8e45eed8268b30f9f7
ibn-salem <jonas.ibn-salem@gmx.de>
2018-01-31 01:35:10 +0100

    add comments

--- _build.sh
+++ _build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# build html version
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+
+# build pdf version
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+
+# build epub version
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
