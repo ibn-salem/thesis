ec898b934889d6b5f0330e9c107bf8d1e1c23042
ibn-salem <jonas.ibn-salem@gmx.de>
2018-02-15 17:38:22 +0100

    update build script to automatically build pdf

--- _build.sh
+++ _build.sh
@@ -3,6 +3,6 @@
 # build book in seprate R sessions for all output formats
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 
-# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
